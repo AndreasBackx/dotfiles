574cd8a83f17e89076ec855700f8bea92b23c54e
Andreas Backx <andreas@backx.org>
2018-07-03 00:12:15 +0200

    Addded nouveau and Nvidia proprietary settings.

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-xrandr --output HDMI-1 --mode 2560x1440 --pos 0x656 --rotate normal --output DP-3 --mode 2560x1440 --pos 2560x0 --rotate right --output DVI-D-1 --off --output DP-1 --off --output DP-2 --off
+# Nouveau
+# xrandr --output HDMI-1 --mode 2560x1440 --pos 0x656 --rotate normal --output DP-3 --mode 2560x1440 --pos 2560x0 --rotate right --output DVI-D-1 --off --output DP-1 --off --output DP-2 --off
+# Nvidia proprietary drivers
+xrandr --output HDMI-0 --mode 2560x1440 --pos 0x656 --rotate normal --output DP-0 --mode 2560x1440 --pos 2560x0 --rotate right --output DVI-D-1 --off --output DP-1 --off --output DP-2 --off
