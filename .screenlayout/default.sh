82ea95151e2c0cae6ca03875a178549aaffb3a67
Andreas Backx <andreas@backx.org>
2019-04-16 15:31:41 +0200

    Removed .screenlayout.

--- .screenlayout/default.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DisplayPort-1 --off --output DisplayPort-0 --primary --rate 144 --mode 2560x1440 --pos 1440x592 --rotate normal --output DisplayPort-3 --mode 2560x1440 --pos 0x50 --rotate left --output DisplayPort-2 --off --output DisplayPort-5 --off --output DisplayPort-4 --mode 2560x1440 --pos 4000x40 --rotate right --output DisplayPort-6 --off --output HDMI-A-0 --off
