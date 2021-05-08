e27e6adece24e2a97b7d30ca4499b6c72145026e
Andreas Backx <andreas@backx.org>
2021-05-08 17:04:44 +0100

    waybar-json and polybar-output used more expansively.

--- .bin/utils.zsh
+++ .bin/utils.zsh
@@ -1,12 +1,28 @@
 #!/usr/bin/env zsh
 
+function is_wayland() {
+    if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+        return 0  # success
+    else
+        return 1  # failure
+    fi
+}
+
 function waybar-json() {
     text=$1
     tooltip=$2
     class=$3
     extra=$4
 
-    echo "{\"text\": \"$text\", \"tooltip\": \"$tooltip\", \"class\": \"$class\"$4}"
+    if [[ -z "$tooltip" ]]; then
+        echo "{\"text\": \"$text\"}"
+    elif [[ -z "$suffix" ]]; then
+        echo "{\"text\": \"$text\", \"tooltip\": \"$tooltip\"}"
+    elif [[ -z "$extra" ]]; then
+        echo "{\"text\": \"$text\", \"tooltip\": \"$tooltip\", \"class\": \"$class\"}"
+    else
+        echo "{\"text\": \"$text\", \"tooltip\": \"$tooltip\", \"class\": \"$class\"$extra}"
+    fi
 }
 
 function waybar-json-percentage() {
@@ -14,10 +30,16 @@ function waybar-json-percentage() {
     waybar-json $1 $2 $3 ", \"percentage\": $percentage"
 }
 
-function is_wayland() {
-    if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
-        return 0  # success
+function polybar-output() {
+    prefix=$1
+    color=$2
+    suffix=$3
+
+    if [[ -z "$color" ]]; then
+        echo "$prefix"
+    elif [[ -z "$suffix" ]]; then
+        echo "%{F$color}$prefix{F-}"
     else
-        return 1  # failure
+        echo "%{F$color}$prefix{F-} $suffix"
     fi
 }
