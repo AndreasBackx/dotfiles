7383d4bb6a28e696ee7da97f0dd4564bd60f37e0
Andreas Backx <andreas@backx.org>
2023-09-06 23:14:11 +0100

    Update .chezmoidata.toml Update README.md Update .config/hypr/monitors.conf Update .config/kanshi/config Update .config/waybar/config Update hyprland.zsh

--- run_after_hyprland.zsh
+++ run_after_hyprland.zsh
@@ -1,5 +1,4 @@
 #!/usr/bin/env zsh
 
-if [ ! -f /usr/share/wayland-sessions/hyprland-shell.desktop ]; then
-  sudo cp ~/.config/hypr/hyprland-shell.desktop /usr/share/wayland-sessions/hyprland-shell.desktop
-fi
+sudo cp ~/.config/hypr/hyprland-shell.desktop /usr/share/wayland-sessions/hyprland-shell.desktop
+
