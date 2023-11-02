09201fdd027f52665476043add00fb28b34f671f
Andreas Backx <andreas@backx.org>
2023-11-02 17:29:48 +0000

    Update .config/discord/settings.json Update hyprland.zsh

--- run_after_hyprland.zsh
+++ run_after_hyprland.zsh
@@ -1,4 +1,9 @@
 #!/usr/bin/env zsh
 
-sudo cp ~/.config/hypr/hyprland-shell.desktop /usr/share/wayland-sessions/hyprland-shell.desktop
+local from=$(realpath ~/.config/hypr/hyprland-shell.desktop)
+local to="/usr/share/wayland-sessions/hyprland-shell.desktop"
 
+cmp --silent $from $to || (
+    echo "$from changed, copying to $to..."
+    sudo cp $from $to
+)
