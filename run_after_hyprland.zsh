939216860df768e12912ad0ea6330f226498847b
Andreas Backx <andreas@backx.org>
2023-11-16 22:09:46 +0000

    Update hyprland.zsh

--- run_after_hyprland.zsh
+++ run_after_hyprland.zsh
@@ -3,6 +3,8 @@
 from=$(realpath ~/.config/hypr/hyprland-shell.desktop)
 to="/usr/share/wayland-sessions/hyprland-shell.desktop"
 
+[ ! -f "$to" ] && exit 0
+
 cmp --silent $from $to || (
     echo "$from changed, copying to $to..."
     sudo cp $from $to
