ab9947766958fc0edef622a147f9d217961a5e8c
Andreas Backx <andreasbackx@meta.com>
2024-06-01 16:10:23 +0100

    Update sass.zsh

--- run_after_sass.zsh
+++ run_after_sass.zsh
@@ -3,4 +3,18 @@
 set -o errexit -o nounset
 setopt BRACE_CCL
 
+if [[ $(uname -s) == "Darwin" ]]; then
+    rm -f ~/.config/waybar/style.css
+    exit 0
+fi
+
+if type "sass" >/dev/null; then
+    command="sass"
+elif type "sassc" >/dev/null; then
+    command="sassc"
+else
+    echo "Install SASS or SASSC to compile the Waybar SCSS style file."
+    exit 1
+fi
+
 sass ~/.config/waybar/style.{scss,css} || sassc ~/.config/waybar/style.{scss,css}
