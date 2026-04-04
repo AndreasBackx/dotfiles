6ae508267ef92c563afec209767fe9188c787918
Andreas Backx <andreas@backx.org>
2026-04-04 11:07:33 +0100

    Update .chezmoidata.toml Update .config/hypr/workspaces.conf Update .config/nvim/lua/config/autocmds.lua Update .config/nvim/lua/config/options.lua Update .config/shikane/config.toml Update .config/sway/config Update .config/waybar/config

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Save all buffers when losing focus
+vim.api.nvim_create_autocmd("FocusLost", {
+  pattern = "*",
+  command = "silent! wall",
+})
