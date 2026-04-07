d1161b50aaf7dcf3a3044d14eb7a278a8e460abc
Andreas Backx <andreas@backx.org>
2026-04-08 00:21:09 +0100

    Add .config/nvim/lua/plugins/blink.lua Add .config/nvim/lua/plugins/neo-tree.lua Add .config/nvim/lua/plugins/snacks.lua Add .config/nvim/lua/plugins/vim-fetch.lua Add .config/nvim/lua/plugins/visual-multi.lua

---
+++ private_dot_config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,16 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      list = {
+        selection = {
+          preselect = false,
+          auto_insert = true,
+        },
+      },
+      menu = {
+        auto_show = false,
+      },
+    },
+  },
+}
---
+++ private_dot_config/nvim/lua/plugins/vim-fetch.lua
@@ -0,0 +1,3 @@
+return {
+  "wsdjeg/vim-fetch",
+}
