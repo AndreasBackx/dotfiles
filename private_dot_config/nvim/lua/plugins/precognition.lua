b9af03925123f5b29f905e95c8f7e4d247910878
Andreas Backx <andreas@backx.org>
2026-06-16 01:47:59 +0100

    Add .config/nvim/lua/plugins/precognition.lua

---
+++ private_dot_config/nvim/lua/plugins/precognition.lua
@@ -0,0 +1,42 @@
+return {
+  "tris203/precognition.nvim",
+  --event = "VeryLazy",
+  opts = {
+    -- startVisible = true,
+    -- debounceMs = 0,
+    -- showBlankVirtLine = true,
+    -- highlightFullVirtLine = false,
+    -- highlightColor = { link = "Comment" },
+    -- targetedMotionHighlightColor = { link = "PrecognitionTargetedMotionDefault" },
+    -- textObjectHighlightColors = {
+    --     { link = "DiffText" },
+    --     { link = "DiffChange" },
+    --     { link = "Visual" },
+    -- },
+    -- targetedMotionHints = {
+    --     enabled = true,
+    --     prio = 1,
+    -- },
+    -- hints = {
+    --      Caret = { text = "^", prio = 2 },
+    --      Dollar = { text = "$", prio = 1 },
+    --      MatchingPair = { text = "%", prio = 5 },
+    --      Zero = { text = "0", prio = 1 },
+    --      w = { text = "w", prio = 10 },
+    --      b = { text = "b", prio = 9 },
+    --      e = { text = "e", prio = 8 },
+    --      W = { text = "W", prio = 7 },
+    --      B = { text = "B", prio = 6 },
+    --      E = { text = "E", prio = 5 },
+    -- },
+    -- gutterHints = {
+    --     G = { text = "G", prio = 10 },
+    --     gg = { text = "gg", prio = 9 },
+    --     PrevParagraph = { text = "{", prio = 8 },
+    --     NextParagraph = { text = "}", prio = 8 },
+    -- },
+    -- disabled_fts = {
+    --     "startify",
+    -- },
+  },
+}
